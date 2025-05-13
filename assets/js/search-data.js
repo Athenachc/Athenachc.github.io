ef2145af81f17a0305cec965df82a720be1c63c6
Athenachc <49759076+Athenachc@users.noreply.github.com>
2025-05-13 17:48:12 +0000

    Deploying to gh-pages from @ Athenachc/Athenachc.github.io@e1dd61a52138637f3f1e603b6186ca49e0c55d12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,14 +110,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%74%68%65%6E%61%63%68%65%75%6E%67%61%63@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/Athenachc", "_blank");
+        },
+      },{
+        id: 'social-gitlab',
+        title: 'GitLab',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://gitlab.com/HKPolyU-UAV", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/athena-cheung-chc", "_blank");
         },
       },{
         id: 'social-rss',
@@ -131,14 +145,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=ffXTcrkAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
